284bf81b67306e7700f9bfd3561888fc7259e375
Jack <jack.oreilly38@mail.dcu.ie>
2020-01-29 17:32:16 +0000

    Set up basic Venue component with audio tag and routing

--- src/juke-app/src/app/app-routing.module.ts
+++ src/juke-app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VenueComponent } from './venue/venue/venue.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'venue', component: VenueComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
