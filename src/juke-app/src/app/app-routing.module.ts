d571dbe1ee0faef3154fca7e78e301eb720f56b5
Jack <jack.oreilly38@mail.dcu.ie>
2020-02-10 13:35:16 +0000

    Work in Perogress using fake backend. Mongo DB will be plugged in. Styles need to be set.

--- src/juke-app/src/app/app-routing.module.ts
+++ src/juke-app/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { VenueComponent } from './venue/venue/venue.component';
-import { AppComponent } from './app.component';
+import { VenueComponent } from './venue/venue.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './_helpers';
 
 
 const routes: Routes = [
-  {path: 'venue', component: VenueComponent },
+  {path: '', component: VenueComponent, canActivate: [AuthGuard] },
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent},
+
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
--- src/juke-app/src/app/app.module.ts
+++ src/juke-app/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VenueComponent } from './venue/venue/venue.component';
+import { VenueComponent } from './venue/venue.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register';
+import { fakeBackendProvider, JwtInterceptor, ErrorInterceptor } from './_helpers';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VenueComponent
+    VenueComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
