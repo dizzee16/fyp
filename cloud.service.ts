2ce84d6705f42bc124a18957964b69f5c0e5ab70
Jack <38988042+dizzee16@users.noreply.github.com>
2020-03-30 22:15:50 +0100

    Changes to fake backend to populate and stream songs

---
+++ cloud.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { of, Subject, Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Song } from '../_interfaces/song';
+import { map, tap } from 'rxjs/operators';
+import { environment } from '@environments/environment';
+
+interface ResponseSongs {
+  results: Song[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CloudService {
+  queue: Subject<any>;
+  private url = `${environment.apiUrl}/assets/songs.json`;
+
+getFiles() {
+    return this.queue.asObservable();
+  }
+
+addToQueue(song) {
+    this.queue.next(song);
+  }
+
+clearQueue() {
+  this.queue.next();
+}
+
+
+constructor(private http: HttpClient) {
+  this.queue = new Subject<Song[]>();
+  }
+
+  postSongs(incoming) {
+    return this.http.post<any>(`${environment.apiUrl}/songs/set`, incoming)
+    .pipe(map(songs => {
+      localStorage.setItem('songs', JSON.stringify(songs));
+      this.queue.next(songs);
+      return songs;
+    }));
+  }
+
+  getSongs(): Observable<any> {
+    return this.http.get(this.url)
+    .pipe(map(response => this.queue.next(response)));
+  }
+}
